75404d59eed9d35d8cf70f8d3d90a56b5664da48
Eric Brandwein <brandweineric@gmail.com>
2018-03-30 20:18:46 -0300

    test_both testea los tres algoritmos

--- test_both.sh
+++ test_both.sh
@@ -5,6 +5,7 @@ do
 	generated=$(python generate-test.py $i)
 	echo $generated | ./fuerza_bruta | read res1
 	echo $generated | ./backtracking | read res2
-	[ $res1 -eq $res2 ] && echo "bien" || \
-	echo -e "mal\n$res1\n$res2\n$generated"
+	echo $generated | ./meet_in_the_middle | read res3
+	[ $res1 -eq $res2 ] && [ $res2 -eq $res3 ] && echo "bien" || \
+	echo -e "mal\n$res1\n$res2\n$res3\n$generated"
 done
