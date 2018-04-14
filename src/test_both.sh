9f5f7b3389bc8706d68692e923e4a58d4a01e222
Eric Brandwein <brandweineric@gmail.com>
2018-04-14 17:18:56 -0300

    Arregla test_both.sh

--- src/test_both.sh
+++ src/test_both.sh
@@ -3,10 +3,10 @@
 for i in {0..15}
 do
 	generated=$(python generate-test.py $i)
-	echo $generated | ./fuerza_bruta | read res1
-	echo $generated | ./backtracking | read res2
-	echo $generated | ./meet_in_the_middle | read res3
-	echo $generated | ./dinamica | read res4
+	res1=$(echo $generated | ./fuerza_bruta)
+	res2=$(echo $generated | ./backtracking)
+	res3=$(echo $generated | ./meet_in_the_middle)
+	res4=$(echo $generated | ./dinamica)
 	[ $res1 -eq $res2 ] && [ $res2 -eq $res3 ] && [ $res3 -eq $res4 ] && \
 	echo "bien" || echo -e "mal\n$res1\n$res2\n$res3\n$generated"
 done
