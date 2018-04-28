402d66ba4c50b117a8d9ca2ad2a235d3475d2a1c
Eric Brandwein <brandweineric@gmail.com>
2018-04-28 16:34:17 -0300

    Capacidades de 50 a 150 y 15 items en test_tiempos_100_capacidad.sh

--- src/test_tiempos_100_capacidad.sh
+++ src/test_tiempos_100_capacidad.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for i in {0..25}; do
-	capacidad=$(( $i * 4))
-	filename="20_items_${capacidad}_capacidad.data"
-	./test_tiempos.sh 20 $capacidad > $filename
+	capacidad=$(( $i * 4 + 50 ))
+	filename="data/15_items_${capacidad}_capacidad.data"
+	./test_tiempos.sh 15 $capacidad > $filename
 	echo "outputeado $filename"
 done
